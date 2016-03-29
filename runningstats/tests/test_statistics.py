d42622c443c8b64edb834cc228d654634ca42db7
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2016-03-29 21:22:59 +0200

    Add test for Statistics.update

--- runningstats/tests/test_statistics.py
+++ runningstats/tests/test_statistics.py
@@ -53,3 +53,24 @@ def test_kurtosis_result():
         s.fill(values)
 
     assert np.isclose(s.kurtosis, kurtosis(data, axis=None))
+
+
+def test_update_result():
+    from .. import Statistics
+    from scipy.stats import skew, kurtosis
+
+    s1 = Statistics()
+    s2 = Statistics()
+
+    data = np.random.normal(1, 2, size=(200, 10))
+
+    for row1, row2 in zip(data[::2], data[1::2]):
+        s1.fill(row1)
+        s2.fill(row2)
+
+    s1.update(s2)
+
+    assert np.isclose(s1.mean, np.mean(data))
+    assert np.isclose(s1.std, np.std(data, ddof=1))
+    assert np.isclose(s1.skewness, skew(data, axis=None))
+    assert np.isclose(s1.kurtosis, kurtosis(data, axis=None))
